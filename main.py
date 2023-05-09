7f8eb530e4a12b50ab9bd032629a607551213092
Peter Spiegler <peter.spiegler@consiliari.de>
2023-05-09 21:19:41 +0200

    Working Bulk: Needs nice Messages and responses

--- main.py
+++ main.py
@@ -1,39 +1,70 @@
 from rivescript import RiveScript
 import os.path
+import re
 import pandas as pd
 import json
 from datetime import date, datetime
+import requests
 
 bot = RiveScript()
 
 
 def __get_content():
+
+    def __remove_parentheses(input):
+        return re.sub("[(\[].*?[)\]]", "", input)
+
+    def __crawl_websites():
+        fileContent = {"crawl_date": date.today().strftime("%d/%m/%Y"), "content": []}
+        content = __crawl_wikipedia() + __crawl_statista()
+        fileContent["content"] = content
+        statistic_file = open("sources.json", "a+")
+        statistic_file.write(json.dumps(fileContent))
+        return statistic_file
+
     def __crawl_wikipedia():
         url = r'https://en.wikipedia.org/wiki/Casualties_of_the_Russo-Ukrainian_War'
         tables = pd.read_html(url)
 
-        wikipedia_source = {"crawl_date": date.today().strftime("%d/%m/%Y"), "content": []}
         wikipedia_data = []
         for entry in tables[2].values:
-            x = {"kind": entry[0], "amount": entry[1], "date_range": entry[2], "source": entry[3]}
+            x = {"kind": __remove_parentheses(entry[0]),
+                 "amount": __remove_parentheses(entry[1]),
+                 "date_range": __remove_parentheses(entry[2]),
+                 "source": __remove_parentheses(entry[3])}
             wikipedia_data.append(x)
 
-        wikipedia_source["content"] = wikipedia_data
+        return wikipedia_data
 
-        wikipedia_file = open("wikipedia_source.json", "w")
-        wikipedia_file.write(json.dumps(wikipedia_source))
-        return wikipedia_file
+    def __crawl_statista():
+        api_key = "tXeTWh-kxYqw"
+        project_token = "tyTjd3PWCqMC"
+
+        params = {
+            'api_key': f'{api_key}',
+        }
+
+        url = f'https://www.parsehub.com/api/v2/projects/{project_token}/last_ready_run/data'
+        response = requests.get(url, params)
+        data = json.loads(response.text)
+        statista_data = [{
+            "kind": "Civilian",
+            "amount": __remove_parentheses(data["civilian_casualties"]),
+            "date_range": __remove_parentheses(data["date_range"]),
+            "source": "Statista"
+        }]
+        return statista_data
 
     try:
-        openedFile = open("wikipedia_source.json", "r")
+        openedFile = open("sources.json", "r+")
     except FileNotFoundError:
         print("No File: Crawling")
-        openedFile = __crawl_wikipedia()
+        openedFile = __crawl_websites()
     else:
-        wikipedia_json = json.loads(openedFile.read())
-        if datetime.strptime(wikipedia_json["crawl_date"], "%d/%m/%Y").date() < date.today():
+        statistic_json = json.loads(openedFile.read())
+        if datetime.strptime(statistic_json["crawl_date"], "%d/%m/%Y").date() < date.today():
             print("File old: Crawling")
-            openedFile = __crawl_wikipedia()
+            openedFile = __crawl_websites()
     openedFile.seek(0, 0)
     return json.loads(openedFile.read())["content"]
 
@@ -66,6 +97,7 @@ def __get_entry(rs, args):
     entry = __get_entries_by_topic(content)[int(index)]
     return f'\n The number of {entry["kind"]} casualties in the timespan from {entry["date_range"]} according to the {entry["source"]} is {entry["amount"]}'
 
+__get_content()
 
 file = os.path.dirname(__file__)
 responses = os.path.join(file, 'responses')
@@ -80,4 +112,4 @@ while True:
     if msg == 'q':
         break
     else:
-        print('Bot>' + bot.reply('localuser', msg))
+        print('Bot> ' + bot.reply('localuser', msg))
